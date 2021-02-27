993580b9f62b40f26c31662016ac44d01a7ded8d
Jakub Pitera <pitjakub@gmail.com>
2021-02-27 15:46:31 +0100

    Configured rabbitmq error handling.

--- chat-messages-service/src/main/java/pl/kubaretip/chatmessagesservice/config/RabbitMQConfig.java
+++ chat-messages-service/src/main/java/pl/kubaretip/chatmessagesservice/config/RabbitMQConfig.java
@@ -1,29 +1,49 @@
 package pl.kubaretip.chatmessagesservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
+    private static final String MESSAGE_STORING_QUEUE = "pl.kubaretip.chatmessagesservice.storing";
+    private static final String MESSAGE_STORING_EXCHANGE = "pl.kubaretip.chatmessagesservice.fanout";
+    private static final String MESSAGE_STORING_DLQ = MESSAGE_STORING_QUEUE + ".dlq";
+    private static final String MESSAGE_STORING_DLE = MESSAGE_STORING_QUEUE + ".dlx";
+
     @Bean
-    public FanoutExchange fanoutExchange() {
-        return new FanoutExchange("pl.kubaretip.chatmessagesservice.fanout");
+    public FanoutExchange messageStoringExchange() {
+        return new FanoutExchange(MESSAGE_STORING_EXCHANGE);
     }
 
     @Bean
     public Queue messageStoringQueue() {
-        return new Queue("pl.kubaretip.chatmessagesservice.storing");
+        return QueueBuilder.durable(MESSAGE_STORING_QUEUE)
+                .deadLetterExchange(MESSAGE_STORING_DLE)
+                .build();
     }
 
     @Bean
-    public Binding bindingActivationMail(FanoutExchange fanoutExchange, Queue usersActivationQueue) {
-        return BindingBuilder.bind(usersActivationQueue).to(fanoutExchange);
+    public Binding messageStoringBinding(Queue messageStoringQueue, FanoutExchange messageStoringExchange) {
+        return BindingBuilder.bind(messageStoringQueue).to(messageStoringExchange);
     }
 
+    @Bean
+    public FanoutExchange deadLetterExchange() {
+        return new FanoutExchange(MESSAGE_STORING_DLE);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable(MESSAGE_STORING_DLQ)
+                .ttl(5000)
+                .build();
+    }
+
+    @Bean
+    public Binding deadLetterBinding(Queue deadLetterQueue, FanoutExchange deadLetterExchange) {
+        return BindingBuilder.bind(deadLetterQueue).to(deadLetterExchange);
+    }
 
 }
