5c47387fef5eaf843d30eca97b54fc5ec55ece01
Jakub Pitera <pitjakub@gmail.com>
2021-03-03 10:50:27 +0100

    test: Added more test cases for UserController class. Changed user details configuration for tests.

--- auth-service/src/test/java/pl/kubaretip/authservice/utils/SpringSecurityWebTestConfig.java
+++ auth-service/src/test/java/pl/kubaretip/authservice/utils/SpringSecurityWebTestConfig.java
@@ -3,13 +3,19 @@ package pl.kubaretip.authservice.utils;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import pl.kubaretip.authutils.security.SecurityUserDetailsImpl;
+import pl.kubaretip.authservice.domain.Authority;
+import pl.kubaretip.authservice.domain.User;
+import pl.kubaretip.authservice.repository.UserRepository;
+import pl.kubaretip.authservice.security.UserDetailsServiceImpl;
 
 import java.util.Collections;
+import java.util.Optional;
 import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 
 @TestConfiguration
@@ -18,10 +24,23 @@ public class SpringSecurityWebTestConfig {
     @Bean
     @Primary
     public UserDetailsService customUserDetailsService() {
-        var user = new SecurityUserDetailsImpl(UUID.randomUUID().toString(),
-                "testUser", "password", true,
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
-        return new InMemoryUserDetailsManager(user);
+
+        var userRepositoryMock = mock(UserRepository.class);
+
+        var user = new User();
+        user.setId(UUID.fromString("1062d618-64f6-401c-ab7c-ef050eb6f4b2"));
+        user.setUsername("testUser");
+        user.setFirstName("userFirstName");
+        user.setLastName("userLastName");
+        user.setEnabled(true);
+        user.setPassword("password");
+        var authority = new Authority();
+        authority.setName("ROLE_USER");
+        user.setAuthorities(Collections.singleton(authority));
+
+        given(userRepositoryMock.findOneWithAuthoritiesByUsernameIgnoreCase(anyString())).willReturn(Optional.of(user));
+
+        return new UserDetailsServiceImpl(userRepositoryMock);
     }
 
 }
--- auth-service/src/test/java/pl/kubaretip/authservice/web/rest/UserControllerTest.java
+++ auth-service/src/test/java/pl/kubaretip/authservice/web/rest/UserControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,6 +23,7 @@ import pl.kubaretip.authservice.messaging.sender.UserSender;
 import pl.kubaretip.authservice.service.UserService;
 import pl.kubaretip.authservice.utils.JacksonIgnoreWriteOnlyAccess;
 import pl.kubaretip.authservice.utils.SpringSecurityWebTestConfig;
+import pl.kubaretip.authservice.web.model.ChangePassRequest;
 import pl.kubaretip.dtomodels.UserDTO;
 import pl.kubaretip.exceptionutils.AlreadyExistsException;
 import pl.kubaretip.exceptionutils.InvalidDataException;
@@ -30,14 +32,14 @@ import pl.kubaretip.exceptionutils.NotFoundException;
 import java.util.stream.Stream;
 
 import static java.util.UUID.randomUUID;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @WebMvcTest(controllers = UserController.class)
@@ -169,7 +171,7 @@ public class UserControllerTest {
 
     @Test
     @WithMockUser
-    public void getUserByIdShouldReturns404WhenUserNotFount() throws Exception {
+    public void getUserByIdShouldReturns404WhenUserNotFound() throws Exception {
 
         // given
         given(userService.findUserById(anyString())).willThrow(NotFoundException.class);
@@ -177,6 +179,121 @@ public class UserControllerTest {
         // when + then
         mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", randomUUID().toString()))
                 .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    @WithMockUser
+    public void getUserByIdShouldReturns200WhenUserExists() throws Exception {
+        // given
+        var id = randomUUID().toString();
+        var userDTO = new UserDTO();
+        userDTO.setId(id);
+        given(userService.findUserById(id)).willReturn(new User());
+        given(userMapper.mapToUserDTO(any(User.class))).willReturn(userDTO);
+
+        // when + then
+        mockMvc.perform(get("/users/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(is(equalTo(id))));
+        verify(userService, times(1)).findUserById(id);
+        verify(userMapper, times(1)).mapToUserDTO(any(User.class));
+    }
+
+    @WithMockUser
+    @Test
+    public void editUserShouldReturns400WhenUserIdIsIncorrect() throws Exception {
+
+        // given
+        var userDTO = new UserDTO();
+        userDTO.setFirstName("name");
+        userDTO.setLastName("surname");
+        given(userService.modifyUser(anyString(), anyString(), anyString())).willThrow(InvalidDataException.class);
+
+        // when + then
+        mockMvc.perform(patch("/users/{id}", randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @WithMockUser
+    @Test
+    public void editUserShouldReturns200WhenSuccessfullyEdited() throws Exception {
+
+        // given
+        given(userService.modifyUser(anyString(), anyString(), anyString())).willReturn(new User());
+        given(userMapper.mapToUserDTO(any(User.class))).willReturn(new UserDTO());
+
+        // when + then
+        mockMvc.perform(patch("/users/{id}", randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new UserDTO())))
+                .andExpect(status().isOk());
+
+    }
+
+
+    @WithUserDetails(userDetailsServiceBeanName = "customUserDetailsService", value = "testUser")
+    @Test
+    public void changePasswordShouldReturns204WhenSuccessfullyChanged() throws Exception {
+        // given (userId comes from userDetailsService)
+        var userId = "1062d618-64f6-401c-ab7c-ef050eb6f4b2";
+        var changePassRequest = new ChangePassRequest();
+        var currentPassword = "1234567";
+        var newPassword = "7654321";
+        changePassRequest.setCurrentPassword(currentPassword);
+        changePassRequest.setNewPassword(newPassword);
+
+        willDoNothing().given(userService).changeUserPassword(userId, currentPassword, newPassword);
+
+        // when + then
+        mockMvc.perform(patch("/users/{id}/change-password", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(changePassRequest)))
+                .andExpect(status().isNoContent());
+        verify(userService, times(1)).changeUserPassword(userId, currentPassword, newPassword);
+    }
+
+    @WithUserDetails(userDetailsServiceBeanName = "customUserDetailsService", value = "testUser")
+    @Test
+    public void changePasswordShouldReturns400WhenIncorrectUserId() throws Exception {
+        // given
+        var userId = "wrong id";
+        var request = new ChangePassRequest();
+        request.setNewPassword("fake_password");
+        request.setCurrentPassword("fake_password");
+
+        // when + then
+        mockMvc.perform(patch("/users/{id}/change-password", userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+        verify(userService, times(0)).changeUserPassword(any(), any(), any());
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidChangePasswordRequestsSource")
+    @WithMockUser
+    public void changePasswordShouldReturns400WhenInvalidInput(ChangePassRequest request) throws Exception {
+
+        // given (in method source)
+        // when + then
+        mockMvc.perform(patch("/users/{id}/change-password", "fakeId")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+        verify(userService, times(0)).changeUserPassword(any(), any(), any());
+    }
+
+    private static Stream<Arguments> invalidChangePasswordRequestsSource() {
+        return Stream.of(
+                Arguments.of(new ChangePassRequest("", "")),
+                Arguments.of(new ChangePassRequest(null, null)),
+                Arguments.of(new ChangePassRequest("1", "1")),
+                Arguments.of(new ChangePassRequest("password", "1")),
+                Arguments.of(new ChangePassRequest("1", "password"))
+        );
     }
 
 
